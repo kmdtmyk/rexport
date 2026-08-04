bb6c233080301ce9755797bf6c1c1563af602557
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-08-05 00:44:55 +0900

    修正漏れ

--- test/rexport/book_exporter_test.rb
+++ test/rexport/book_exporter_test.rb
@@ -5,14 +5,14 @@ class Rexport::BookExporterTest < ActiveSupport::TestCase
   test 'to_csv' do
     Book.create!(
       id: 1,
-      name: 'book1',
+      title:  'book1',
       price: 100,
       release_date: Date.new(2026, 8, 6),
       created_at: Time.new(2026, 8, 1, 2, 3, 4),
       updated_at: Time.new(2026, 8, 9, 8, 7, 6),
     )
     assert_equal BookExporter.new(Book.all).to_csv, <<~CSV
-      "id","名前","価格","発売日","created_at","updated_at"
+      "id","タイトル","価格","発売日","created_at","updated_at"
       "1","book1","100","2026/08/06","2026/08/01 02:03:04","2026/08/09 08:07:06"
     CSV
   end
