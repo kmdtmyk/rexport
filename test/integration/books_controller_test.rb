3f91e7cc708b4c64ba20cee6123ae3f9a46318ab
kmdtmyk <winback_banderas@yahoo.co.jp>
2026-08-05 01:07:14 +0900

    responseヘッダのテスト追加

--- test/integration/books_controller_test.rb
+++ test/integration/books_controller_test.rb
@@ -7,6 +7,8 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     get '/books.csv'
     assert_response :success
     assert_equal 'text/csv', response.media_type
+    assert_match /attachment/, response.headers['Content-Disposition']
+    assert_match /books\.csv/, response.headers['Content-Disposition']
     rows = CSV.parse(response.body, headers: true)
     assert_equal 1, rows.size
     assert_equal 'Rails入門', rows[0]['タイトル']
